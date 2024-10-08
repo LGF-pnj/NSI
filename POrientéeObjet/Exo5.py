3fa08b8fc6c1ac93b6229dec220587ff0aa0cb56
LGF-pnj <lorna64fernandez@gmail.com>
2024-10-08 20:54:48 +0200

    Update Exo5.py : finished

--- POrientéeObjet/Exo5.py
+++ POrientéeObjet/Exo5.py
@@ -10,3 +10,31 @@ def imin(self):
     if mini!=0:
         mini=-mini
     return mini
+
+def imax(self):
+    return len(self.droite)-1
+
+def append(self, elt):
+    self.droite.append(elt)
+
+def prepend(self, elt):
+    self.gauche.append(elt)
+
+def __getitem__(self, i):
+    if i<0 :
+        return self.gauche[-i-1]
+    else :
+        return self.droite[i]
+    
+def __setitem__(self, i, elt):
+    if i<0 and i<len(self.gauche):
+        self.gauche[i]=elt
+    elif i>=0 and i>=len(self.droite)-1 :
+        self.droite[i]=elt
+
+def __str__(self):
+    Btab=[]
+    for elem in self.gauche[ : :-1]:
+        Btab.append(elem)
+    Btab.extend(self.droite)
+    return Btab
