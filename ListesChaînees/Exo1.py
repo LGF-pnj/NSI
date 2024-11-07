f69cd95ab9a8332dfb6078701454ce18aa4d6ca4
LGF-pnj <lorna64fernandez@gmail.com>
2024-11-07 11:02:10 +0100

    Update Exo1.py

--- ListesChaînees/Exo1.py
+++ ListesChaînees/Exo1.py
@@ -1,3 +1,13 @@
 from ListeImport import *
 from FractonsImport import *
 from random import randint
+
+lst=Liste()
+for i in range(6):
+  num=randint(-10, 10)
+  denom= randint(1,10)
+  f=Fraction(num, denom)
+  lst.ajoute(f)
+#créee une liste chaînée de 6 fractions
+
+print(lst)
