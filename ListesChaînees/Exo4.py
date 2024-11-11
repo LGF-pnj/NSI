133e9ef9469f5d7ec1c13d6ccc17409083cd963e
LGF-pnj <lorna64fernandez@gmail.com>
2024-11-11 09:33:56 +0100

    Update Exo4.py

--- ListesChaînees/Exo4.py
+++ ListesChaînees/Exo4.py
@@ -1,15 +1,23 @@
 from ListeImport import Cellule
 
 def trouverec(x,lst):
-    if lst is None :
+    if lst==None:
+        return None
+    if lst.valeur == x :
         return 0
-    elif lst.valeur == x :
-        return trouverec(x, lst.suivante)
     else :
-        return 1+trouverec(x, lst.suivante)
+            if trouverec(x, lst.suivante) !=None :
+                return 1+trouverec(x, lst.suivante) 
+            else :
+                return None
+        
     
 def trouvewh(x ,lst):
     i=0
     while lst.valeur != x:
         i+=1
         lst=lst.suivante
+    if i!=0:
+        return i
+    else :
+        return None
