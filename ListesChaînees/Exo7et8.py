7c87ae8013b118b0ec6e84d055737b4484d0ec70
LGF-pnj <lorna64fernandez@gmail.com>
2024-11-13 16:48:33 +0100

    Update Exo7et8.py : exos 9 et 10

--- ListesChaînees/Exo7et8.py
+++ ListesChaînees/Exo7et8.py
@@ -10,27 +10,35 @@ def inserer(x,lst):
         return inserer(x, lst.suivante)
     
 
-
-
 def tri_par_insertion(lst):
     if lst is None :
         return None
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #"kepler!"
+
+#correction en photo (?) sinon va voir sur ed
+#aussi bon courage
+#you got this
+
+
+
+#exos 9 et 10 lol
+
+#9
+from ListeImport import Cellule
+
+def listeN(n):
+    if n==0:
+        return None
+    elif n==1:
+        return 1
+    else:
+        return Cellule(listeN(n-1), n)
+
+
+#10
+
+def liste_tableau(t):
+    lchai=None
+    for i in range(0,len(t),-1):
+        lchai=Cellule(t[i], lchai)
