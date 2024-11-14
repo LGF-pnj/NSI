8e6e15ebe5e38784daad0a1d9071b2c817e3afb4
L.Fernandez <LornaFernandez@email.com>
2024-11-14 12:07:05 +0100

    pile et file debut

---
+++ Piles et files/Pile.py
@@ -0,0 +1,23 @@
+class Cellule:
+    def __init__(self, v, s):
+        self.valeur=v
+        self.suivante=s
+
+class Pile:
+    def __init__(self):
+        self._contenu = None
+
+    def est_vide(self):
+        return self._contenu is None
+    
+    def empiler(self, e):
+        self._contenu =  Cellule(e, self._contenu)
+
+    def depiler(self):
+        if self.est_vide():
+            raise IndexError("Depile une pile vide")
+        v=self._contenu.valeur
+        self._contenu=self._contenu.suivante
+
+def creer_pile():
+    return Pile()
