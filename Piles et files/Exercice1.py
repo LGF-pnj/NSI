3e37e0f0048a1012c1dd05a318b3517beb2598bf
LGF-pnj <lorna64fernandez@gmail.com>
2024-11-16 19:25:07 +0100

    Update Exercice1.py

--- Piles et files/Exercice1.py
+++ Piles et files/Exercice1.py
@@ -1,20 +1,31 @@
+
 from Pile import*
 
 class Historique:
     def __init__(self):
         self.adresse_courante=None
         self._adresses_precedentes=creer_pile()
+        self._adresses_suivantes=creer_pile()
 
     def aller_a(self, adr):
         if not self.adresse_courante is None:
             self._adresses_precedentes.empiler(self.adresse_courante)
         self.adresse_courante = adr
+        self._adresses_suivantes=creer_pile()
 
     def retour(self):
         if not self._adresses_precedentes.est_vide():
+            self._adresses_suivantes = self._adresses_suivantes.empiler(self.adresse_courante)
             self.adresse_courante = self._adresses_precedentes.depiler()
         else:
             self.adresse_courante = None
+
+    def retour_avant(self):
+        if not self._adresses_suivantes.est_vide():
+            self.adresse_courante = self._adresses_suivantes.depiler()
+        else :
+            raise KeyError("pas d'adresses suivantes")
+
 
 h=Historique()
 assert h.adresse_courante is None
@@ -48,4 +59,3 @@ assert h.adresse_courante == "www.stackoverflow.com"
 h.aller_a("www.w3schools.com")
 h.aller_a("www.developer.com")
 h.retour()
-
