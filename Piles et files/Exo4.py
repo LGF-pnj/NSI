ed9cb0d0b493bee37fab69c8947018136656cb8f
LGF-pnj <lorna64fernandez@gmail.com>
2024-11-18 19:10:49 +0100

    Update Exo4.py

--- Piles et files/Exo4.py
+++ Piles et files/Exo4.py
@@ -1,9 +1,16 @@
 from Pile import*
 
-#parenthesee
+#fn parenthesee
 def egeeEthra(ch, f):
-    chch=""
-    for char in ch:
-        if char=="(" or char==")":
-            chch+=char
-        else :
+    chpile=creer_pile()
+    for i in range (len(ch)):
+        if ch[i]=="(" or ch[i]==")":
+            if chpile.est_vide():
+                chpile.empiler(i)
+            else :
+                chpile.depiler()
+                chpile.empiler(i)
+        elif i == f:
+            return chpile.depiler()
+        
+print(egeeEthra("(45+25)*((12+14)+4)", 15))
