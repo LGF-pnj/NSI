b77b68b8ac84bf420047d0f86869650d53d1d9ef
LGF-pnj <lorna64fernandez@gmail.com>
2024-12-09 15:48:26 +0100

    Update Exo1.py

--- Arborescences/Exo1.py
+++ Arborescences/Exo1.py
@@ -8,3 +8,6 @@ def affiche(a, ch):
         print(affiche(f, ch))
     
 affiche(a, "    ")
+
+#on peut mettre une valeir par defaut a ch ds. la def de fn --> def affiche(a, ch="")
+#aussi on ne ceut pas un espace mais une chaîne vide.
