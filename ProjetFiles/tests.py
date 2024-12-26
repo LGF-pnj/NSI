fb1296f635ec7f24bab244b3046ec4821493b264
LGF-pnj <lorna64fernandez@gmail.com>
2024-12-26 10:14:00 +0100

    Update tests.py

--- ProjetFiles/tests.py
+++ ProjetFiles/tests.py
@@ -1,26 +1,39 @@
-# si on a le temps : methode superconfig pr lancer plusieur simuatons et faire des stats
+# si on a le temps : methode superconfig pr lancer plusieurs simulations et faire des stats
 from file import *
 from Main import *
 from simulateur import *
-
 
 class FakeSimulateur:
     def __init__(self):
         self._totalAttente = 0
         self._tick = 0
+        self.nbGuichets = 5
 
-s1 = FakeSimulateur()
-rep_alea = RepartiteurAlea(s1)
+s2 = FakeSimulateur()
+repAl = RepartiteurAlea(s2)
 
 
+s3 = FakeSimulateur()
+repCh = RepartiteurChoix(s3)
 
-
-e=File()
-print(len(e))
-e.ajouter(4)
-e.ajouter(3)
-e.ajouter(6)
-print(len(e))
-e.retirer()
-print(len(e))
-e.ajouter(3)
+repCh.entree_client()
+s3._tick+=1
+repCh.entree_client()
+s3._tick+=1
+repCh.entree_client()
+s3._tick+=1
+repCh.entree_client()
+s3._tick+=1
+repCh.entree_client()
+s3._tick+=1
+repCh.entree_client()
+s3._tick+=1
+repCh.entree_client()
+s3._tick+=1
+repCh.entree_client()
+s3._tick+=1
+print(repCh.files[0])
+print(repCh.files[1])
+print(repCh.files[2])
+print(repCh.files[3])
+print(repCh.files[4])
