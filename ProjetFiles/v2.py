bb08fbcb1f247e1410ff7407d8b4a16615d91547
LGF-pnj <lorna64fernandez@gmail.com>
2025-01-12 18:30:09 +0100

    Update v2.py

--- ProjetFiles/v2.py
+++ ProjetFiles/v2.py
@@ -2,46 +2,74 @@ from file import *
 from Main import *
 from simulateur import *
 
-#tick pas en param de entree client ds. les repartiteurs : pk ? justifier, discuter à l'oral
-
-#documentation !
-
-#(elucider la preference de repartiteurchoix)
-
-#methode superconfig pr lancer plusieur simulations et faire des stats
-
-# rajouter vérifs, prog.defensive + interface sympathique pour fn config
 #client : pk creerclient ? si inutile replacer par appel simple Client(tick)
+
 #variable ds.les files(class File) qui garde trace de la longueur pour eviter de len() 
 #... systématiquement ds. repartiteurchoix
 
+#documentation !
 
+#tick pas en param de entree client ds. les repartiteurs : pk ? justifier, discuter à l'oral
 
-#rajouter les instances guichet ds. la liste des guichets + l'instance du bon repartiteur repartiteur dans l'attribut repartiteur
+#(elucider la preference de repartiteurchoix)
 
 def configv2(self):
+    """configure le simulateur selon les inputs des utilisateurs - v2"""
     print("Configuration du simulateur")
     print("nombres de tours de la simulation ?")
-    #vérif que input intable + que entier pos.
-    self.nbTours = int(input())
+    self.nbTours = int_valide(input())
+    if self.nbTours == 0 :
+        self.nbTours = reesayez(self.nbTours, int_valide, "Entrée invalide. Veuillez entrer un nombre strictement positif.")
     print("Nombre de guichets ?")
-    print("[Entier attendu. Exemple : 12]")
-    #vérif que input intable + que entier pos.
-    self.nbGuichets=int(input())
+    nbGuichets = int_valide(input())
+    if nbGuichets == 0 :
+        nbGuichets = reesayez(nbGuichets, int_valide, "Entrée invalide. Veuillez entrer un nombre strictement positif.")
+    for i in range(nbGuichets) :
+        self.guichets.append(Guichet(self, i+1))
     print("temps de traitement maximal d'un client aux guichets, en nombre de tours ?")
-    print("[Entier attendu. Exemple : 8]")
-    #vérif que input intable + que entier pos. + que sup.0 et inf. nbtours
-    self.max = int(input())
-    if self.nbGuichets > 1 :
+    #vérif que input inf. nbtours ?
+    self.max = int_valide(input())
+    if self.max == 0 :
+        self.max = reesayez(self.max, int_valide, "Entrée invalide. Veuillez entrer un nombre strictement positif.")
+    if len(self.guichets) > 1 :
         print("Mode de répartition des clients ?")
         print(" - 'Un' : une file d'attente unique partagée par tous les clients")
         print(" - 'Aleatoire' : plusieurs files d'attentes, le client choisit aléatoirement entre elles ")
         print(" - 'Choix' : plusieurs files d'attentes, les clients choisissent la plus courte")
-        #vérif que correspond bien à un des trois + modif. pr. avoir le bon appel (abandon de self.repartiteurs)
-        self._repartiteur=self.repartiteurs[input()]
+        self._repartiteur=mode_rep_valide(input(), self)
+        if self._repartiteur == None :
+            self._repartiteur = reesayezRep(self._repartiteur, mode_rep_valide, "Entrée invalide. Veuillez entrer une des possibilités proposées.", self, None)
     else :
         self._repartiteur=RepartiteurUn(self)
 
 
-def int_valide(elem):
+def int_valide(elem : str):
+    if elem.isdigit() and int(elem)>0 :
+        return int(elem)
+    return 0
+        
+def mode_rep_valide(elem : str, self):
+    if elem == "Un" : 
+        return RepartiteurUn(self)
+    if elem == "Aleatoire" : 
+        return RepartiteurAlea(self)
+    if elem == "Choix" : 
+        return RepartiteurChoix(self)
+    
+def reesayez(attribut, validation, erreur : str, valAttribut=0) :
+        while attribut == valAttribut :
+            print(erreur)
+            attribut = validation(input())
+        return attribut
+
+def reesayezRep(attribut, validation, erreur : str, self, valAttribut=0) :
+        while attribut == valAttribut :
+            print(erreur)
+            attribut = validation(input(), self)
+        return attribut
+
+
+#methode superconfig pr lancer plusieur simulations et faire des stats
+
+def superconfig():
     pass
